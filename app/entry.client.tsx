735439540dde401b38791bce94a5ef5888524e60
Anton Yanovskyi <3898110@gmail.com>
2023-03-28 18:21:20 +0300

    - add Sentry

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,13 +1,14 @@
 import { RemixBrowser, useLocation, useMatches  } from "@remix-run/react";
 import { startTransition, StrictMode, useEffect } from "react";
 import * as Sentry from "@sentry/remix";
+import { Integrations } from '@sentry/tracing';
 import { hydrateRoot } from "react-dom/client";
 
 Sentry.init({
   dsn: "https://bd32a7ac0ac64c5f80fd25bd727f5c79:b0f0f265ff2445e38b4a08641c5c5d35@o4504576276037632.ingest.sentry.io/4504916542488576",
   tracesSampleRate: 1,
   integrations: [
-    new Sentry.BrowserTracing({
+    new Integrations.BrowserTracing({
       routingInstrumentation: Sentry.remixRouterInstrumentation(
           useEffect,
           useLocation,
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -6,12 +6,13 @@ import isbot from "isbot";
 import * as Sentry from "@sentry/remix";
 import { useEffect } from "react";
 import { renderToPipeableStream } from "react-dom/server";
+import { Integrations } from '@sentry/tracing';
 
 Sentry.init({
       dsn: "https://bd32a7ac0ac64c5f80fd25bd727f5c79:b0f0f265ff2445e38b4a08641c5c5d35@o4504576276037632.ingest.sentry.io/4504916542488576",
     tracesSampleRate: 1,
     integrations: [
-        new Sentry.BrowserTracing({
+        new Integrations.BrowserTracing({
             routingInstrumentation: Sentry.remixRouterInstrumentation(
                 useEffect,
                 useLocation,
